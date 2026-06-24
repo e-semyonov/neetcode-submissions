fd6d8c0bd7102e3327d3d27727b5a16270f31c0f
e-semyonov <e.semyonov@gmail.com>
2026-06-25 00:30:14 +0300

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        char[] charArrays = s.toCharArray();
+        char[] charArrayt = t.toCharArray();
+
+        if (charArrays.length != charArrayt.length) {
+            return false;
+        }
+
+        HashMap<Character, Integer> charCountMapS = new HashMap<Character, Integer>();
+        for (char c : charArrays) {
+            charCountMapS.put(c, charCountMapS.getOrDefault(c, 0) + 1);
+        }
+        HashMap<Character, Integer> charCountMapT = new HashMap<Character, Integer>();
+        for (char c : charArrayt) {
+            charCountMapT.put(c, charCountMapT.getOrDefault(c, 0) + 1);
+        }
+
+        if (charCountMapS.size() != charCountMapT.size()) {
+            return false;
+        }
+
+        //compare the maps
+        for (Map.Entry<Character, Integer> entry : charCountMapS.entrySet()) {
+
+            Character key = entry.getKey();
+            Integer value = entry.getValue();
+            Integer remove = charCountMapT.remove(key);
+            if (remove == null) return false; // character from s not found in t
+            if (!remove.equals(value)) return false; // character count does not match
+        }
+
+        return charCountMapT.isEmpty();
+    }
+}
